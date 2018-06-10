04f44c485e43fe55990e71c4ee1c74c308f51b19
Chris Atkin <atkinchris@gmail.com>
2018-06-10 19:11:08 +0100

    Remove Generic Access to prevent conflicts with OSX

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,9 @@
-const GenericAccess = require('./genericAccess')
 const HID = require('./hid')
 const Battery = require('./battery')
 const DeviceInformation = require('./deviceInformation')
 
 const buildServices = () => {
   const services = [
-    new GenericAccess(),
     new HID(),
     new Battery(),
     new DeviceInformation(),
